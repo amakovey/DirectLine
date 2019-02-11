14fb2fe4d1078c8dbf2a461053998b21fba06b26
amakovey <46588607+amakovey@users.noreply.github.com>
2019-02-11 16:30:45 +0400

    Add files via upload

--- loto.py
+++ loto.py
@@ -5,7 +5,7 @@ import os
 # Создаём класс игровой карточки
 class GameCard:
     def __init__(self):
-        a = [[0] * 9 for i in range(3)]
+        a = [[0] * 9 for i in range(3)]  # Массив игровой карточки для заполнения
         # Создаем массив всех возможных номеров в карточке
         card_numbers = [[i for i in range(1, 10)],
                         [i for i in range(10, 20)],
@@ -17,21 +17,19 @@ class GameCard:
                         [i for i in range(70, 80)],
                         [i for i in range(80, 91)]]
         # Заполняем карточку номерами
-        for j in range(0, 3, 1):
-            i = 0
-            while i < 9:
-                n = random.choice(card_numbers[i])
-                if n != 0:
-                    card_numbers[i][card_numbers[i].index(n)] = 0
-                    a[j][i] = n
-                    i += 1
+        for j in range(3):
+            for i in range(9):
+                n = random.choice(card_numbers[i])  # Выбираем число из соотвествующего подмассива
+                a[j][i] = n
+                card_numbers[i].pop(card_numbers[i].index(n))  # Избегаем повтора в карточке
+
         # Расставляем рандомно пустые клетки в карточку
-        for j in range(0, 3, 1):
+        for j in range(3):
             i = 0
-            while i < 4:
+            while i < 4:  # Повторяем цикл 4 раза, пока в каждой из строк карточки не проставим 4 пустых клетки (9-5=4)
                 c = random.randint(0, 8)
                 if a[j][c] != 0:
-                    a[j][c] = 0
+                    a[j][c] = 0  # 0 - признак пустой ячейки
                     i += 1
         self.card = a
 
@@ -52,13 +50,14 @@ class GameCard:
     # Поиск номера бочонка в карточке
     def search_num(self, n):
         s = 0
-        for i in range(0, 3, 1):
-            for j in range(0, 9, 1):
+        for i in range(3):
+            for j in range(9):
                 if self.card[i][j] == n:
                     s = 1
-                    self.card[i][j] = 99
+                    self.card[i][j] = 99  # 99 - признак найденной и зачеркнутой ячейки
         return s
 
+    # Обработка правильности ввода y/n
     def input_yn(n, turn):
         print('Новый бочонок:', n, '(осталось', str(90 - turn) + ')')
         a = input('зачеркнуть цифру? (y/n) ')
@@ -69,7 +68,7 @@ class GameCard:
             GameCard.input_yn(n, turn)
 
 
-keg = [i for i in range(1, 91, 1)]
+keg = [i for i in range(1, 91, 1)]  # Массив бочонков
 my_card, comp_card = GameCard(), GameCard()
 my_score, comp_score, turn = 0, 0, 1
 
@@ -84,7 +83,7 @@ while turn < 91:
     n = random.choice(keg)
     yn = GameCard.input_yn(n, turn)
     print()
-    keg.pop(keg.index(n))
+    keg.pop(keg.index(n))  # Удаляем бочонок из массива
     turn += 1
     m = my_card.search_num(n)
     if m == 1 and yn == 'y':
